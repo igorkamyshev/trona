8406aee5992c195c3f7637bf687207ab2bf64031
uka17 <ukaoneseven@gmail.com>
2021-10-22 00:42:33 +0300

    Check down script option was added

--- bin.js
+++ bin.js
@@ -19,117 +19,131 @@ sade('trona', true)
   .option('-y, --no-interactive', 'Skip interactive questions', false)
   .option('-c, --config-path', 'Custom path to config', null)
   .option('-d, --evolutions-dir', 'Custom evolutions directory', null)
-  .action(async ({ y: noInteractive, c: configPath, d: evolutionsDirPath }) => {
-    try {
-      const { tableName, runQuery, evolutionsPath } = await getConfig({
-        configPath,
-        evolutionsDirPath,
-      });
-
+  .option('-k, --check-down', 'Check down script', false)
+  .action(
+    async ({
+      y: noInteractive,
+      c: configPath,
+      d: evolutionsDirPath,
+      k: checkDownScript,
+    }) => {
       try {
-        const modifiedQuery = createInitTableQuery(tableName);
-
-        print(kleur.yellow(`Evolution's table '${tableName}' init started`));
-
-        await runQuery(modifiedQuery);
-
-        print(kleur.green('Evolutions table is OK'));
-      } catch (error) {
-        print(
-          kleur.red('An error occured during initialization:'),
-          kleur.red(error.message),
-        );
-
-        throw error;
-      }
-
-      const { evolutions, outOfScopeFiles } = await getEvolutions(
-        evolutionsPath,
-      );
-
-      if (outOfScopeFiles.length > 0)
-        print(
-          kleur.yellow(
-            `Warning!. Next evolution files are out of scope because of incorrect naming:\n${outOfScopeFiles
-              .map((x) => `- ${x}`)
-              .join('\n')}`,
-          ),
-        );
-
-      const executedEvolutionInfo = await runQuery(
-        `SELECT id, checksum, down_script FROM ${tableName} ORDER BY id ASC;`,
-      );
-
-      const firstInconsistentEvolutionId = findInconsistentEvolutionId({
-        newEvolutions: evolutions,
-        oldEvolutions: executedEvolutionInfo,
-      });
-
-      if (firstInconsistentEvolutionId == null) {
-        print(kleur.green('All your database evolutions already consistent'));
-        process.exit(0);
-      }
-
-      print(
-        kleur.yellow(
-          `Your first inconsistent evolution is ${firstInconsistentEvolutionId}.sql`,
-        ),
-      );
-
-      const evolutionsToDegrade = getInconsistentEvolutions(
-        executedEvolutionInfo,
-        firstInconsistentEvolutionId,
-      );
-
-      if (evolutionsToDegrade.length !== 0) {
-        print(
-          kleur.yellow(
-            `There are ${evolutionsToDegrade.length} inconsistent evolutions`,
-          ),
-        );
-      }
-
-      for (const { down_script: downScript, id } of evolutionsToDegrade) {
-        print(kleur.yellow(`--- ${id}.sql ---`), kleur.yellow(downScript));
-
-        await confirmOperation({
-          question: 'Do you wish to run this degrade script?',
-          noInteractive,
+        const { tableName, runQuery, evolutionsPath } = await getConfig({
+          configPath,
+          evolutionsDirPath,
         });
 
-        await runQuery(downScript);
-        await runQuery(`DELETE FROM ${tableName} WHERE id = ${id};`);
-      }
+        try {
+          const modifiedQuery = createInitTableQuery(tableName);
 
-      const evolutionsToExecute = getInconsistentEvolutions(
-        evolutions,
-        firstInconsistentEvolutionId,
-      );
+          print(kleur.yellow(`Evolution's table '${tableName}' init started`));
 
-      if (evolutionsToExecute.length !== 0) {
-        print(kleur.blue('Running evolve scripts'));
-      }
+          await runQuery(modifiedQuery);
 
-      for (const { data, id, checksum } of evolutionsToExecute) {
-        const [upScript, downScript] = data.split('#DOWN');
+          print(kleur.green('Evolutions table is OK'));
+        } catch (error) {
+          print(
+            kleur.red('An error occured during initialization:'),
+            kleur.red(error.message),
+          );
 
-        print(kleur.blue(`--- ${id}.sql ---`), kleur.blue(upScript));
+          throw error;
+        }
 
-        await runQuery(upScript);
-
-        await runQuery(
-          `INSERT INTO ${tableName} (id, checksum, down_script) VALUES (${id}, '${checksum}', ${
-            downScript ? `'${downScript.replace(/'/g, "''").trim()}'` : 'NULL'
-          });`,
+        const { evolutions, outOfScopeFiles } = await getEvolutions(
+          evolutionsPath,
         );
+
+        if (outOfScopeFiles.length > 0)
+          print(
+            kleur.yellow(
+              `Warning!. Next evolution files are out of scope because of incorrect naming:\n${outOfScopeFiles
+                .map((x) => `- ${x}`)
+                .join('\n')}`,
+            ),
+          );
+
+        const executedEvolutionInfo = await runQuery(
+          `SELECT id, checksum, down_script FROM ${tableName} ORDER BY id ASC;`,
+        );
+
+        const firstInconsistentEvolutionId = findInconsistentEvolutionId({
+          newEvolutions: evolutions,
+          oldEvolutions: executedEvolutionInfo,
+        });
+
+        if (firstInconsistentEvolutionId == null) {
+          print(kleur.green('All your database evolutions already consistent'));
+          process.exit(0);
+        }
+
+        print(
+          kleur.yellow(
+            `Your first inconsistent evolution is ${firstInconsistentEvolutionId}.sql`,
+          ),
+        );
+
+        const evolutionsToDegrade = getInconsistentEvolutions(
+          executedEvolutionInfo,
+          firstInconsistentEvolutionId,
+        );
+
+        if (evolutionsToDegrade.length !== 0) {
+          print(
+            kleur.yellow(
+              `There are ${evolutionsToDegrade.length} inconsistent evolutions`,
+            ),
+          );
+        }
+
+        for (const { down_script: downScript, id } of evolutionsToDegrade) {
+          print(kleur.yellow(`--- ${id}.sql ---`), kleur.yellow(downScript));
+
+          await confirmOperation({
+            question: 'Do you wish to run this degrade script?',
+            noInteractive,
+          });
+
+          await runQuery(downScript);
+          await runQuery(`DELETE FROM ${tableName} WHERE id = ${id};`);
+        }
+
+        const evolutionsToExecute = getInconsistentEvolutions(
+          evolutions,
+          firstInconsistentEvolutionId,
+        );
+
+        if (evolutionsToExecute.length !== 0) {
+          print(kleur.blue('Running evolve scripts'));
+        }
+
+        for (const { data, id, checksum } of evolutionsToExecute) {
+          const [upScript, downScript] = data.split('#DOWN');
+
+          print(kleur.blue(`--- ${id}.sql ---`), kleur.blue(upScript));
+
+          await runQuery(upScript);
+
+          if (!checkDownScript) {
+            print(kleur.blue('Checking down script'));
+            await runQuery(downScript);
+            await runQuery(upScript);
+          }
+
+          await runQuery(
+            `INSERT INTO ${tableName} (id, checksum, down_script) VALUES (${id}, '${checksum}', ${
+              downScript ? `'${downScript.replace(/'/g, "''").trim()}'` : 'NULL'
+            });`,
+          );
+        }
+
+        print(kleur.green('Evolution is successful!'));
+
+        process.exit(0);
+      } catch (error) {
+        print(kleur.red('Fatal error:'), kleur.red(error.message));
+        process.exit(1);
       }
-
-      print(kleur.green('Evolution is successful!'));
-
-      process.exit(0);
-    } catch (error) {
-      print(kleur.red('Fatal error:'), kleur.red(error.message));
-      process.exit(1);
-    }
-  })
+    },
+  )
   .parse(process.argv);
